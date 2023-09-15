2c983723c93ffc5725dd63a92dc53739a252ec42
João Camargo <jclaudio00_12@hotmail.com>
2023-09-15 16:51:08 -0300

    day one

--- src/main/java/br/com/testandowebjava/PrimeiraServlet.java
+++ src/main/java/br/com/testandowebjava/PrimeiraServlet.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//@WebServlet("/segundaServlet")
+@WebServlet(urlPatterns = {"/primeiraServlet", "/segundaServlet", "/xpto", "/teste"})
 public class PrimeiraServlet extends HttpServlet {
 
     @Override
